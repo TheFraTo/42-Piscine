f5679a69829e3bbdf0dd6c448f70d68a17abd01c
FraTo <61939997+TheFraTo@users.noreply.github.com>
2026-07-24 10:08:44 +0200

    C05
    ex00: OK | ex01: OK | ex02: OK | ex03: OK | ex04: OK | ex05: OK | ex06: OK

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/24 10:06:30 by username          #+#    #+#             */
+/*   Updated: 2026/07/24 10:06:30 by username           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	result = 1;
+	while (nb > 1)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_iterative_factorial(5));
+// 	return (0);
+// }
---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/22 16:27:54 by username          #+#    #+#             */
+/*   Updated: 2026/07/23 12:02:35 by username           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_recursive_factorial(5));
+// 	return (0);
+// }
